9737b1fff97fe8ee9f36129e1b85973073e1c5af
titomon98 <arturomonterroso3@gmail.com>
2021-07-05 20:19:39 -0600

    Configurando base de datos

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,11 @@
-const express =require('express');
-const router =express.Router();
+const express = require('express');
+const router = express.Router();
 
 // Importo el controlador
 const proyectoController = require('../controllers/proyectoController');
 
-module.exports = function(){ 
+module.exports = function () {
     // ruta para el home
-    router.get('/',proyectoController.proyectoHome);
+    router.get('/', proyectoController.proyectoHome);
     return router;
 }
-
-
-
