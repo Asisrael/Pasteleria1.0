0ae7d7a9972294a8ae74e708edebd2cecf307d2b
Asisrael <45052301+Asisrael@users.noreply.github.com>
2021-07-07 21:03:29 -0700

    Modelos

---
+++ config/models/caja/RetiroCajas.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+const RetiroCajasSchema = mongoose.Schema({
+    registro: {
+        type: Date,
+        default: Date.now()
+    },
+    actualizacion: {
+        type: Date,
+        default: Date.now()
+    },
+    estado: {
+        type: String,
+        trim: true,
+        required: true,
+        default: 'ACTIVO',
+    },
+    caja_chica: {
+        type: Schema.ObjectId,
+        ref: 'CajaChicas',
+        required: true,
+    }
+});
+
+module.exports = mongoose.model('RetiroCajas', RetiroCajasSchema);
